df1b26f1de5f3de6f71ac6abc080f49611e33571
Matin <65161829+Matin2@users.noreply.github.com>
2024-04-17 13:51:03 +0330

    Internal Changes

--- src/main/kotlin/me/matin/core/managers/Config.kt
+++ src/main/kotlin/me/matin/core/managers/Config.kt
@@ -8,36 +8,32 @@ import kotlin.io.path.Path
 import kotlin.io.path.createFile
 import kotlin.io.path.createParentDirectories
 
-class Config(plugin: Plugin, private var config: String) {
+class Config(plugin: Plugin, config: String) {
 
-    init {
-        if (!config.endsWith(".yml")) config = "$config.yml"
-        while (config.startsWith('/')) {
-            config = config.removePrefix("/")
+    private var conf: String = config
+        get() {
+            while (field.startsWith('/')) {
+                field = field.removePrefix("/")
+            }
+            if (!field.endsWith(".yml")) return "$field.yml".trim()
+            return field.trim()
         }
-    }
 
-    private val path = Path("${plugin.dataFolder.path}/$config")
+    private val path = Path("${plugin.dataFolder.path}/${conf}")
     private lateinit var configFile: FileConfiguration
 
     @Throws(IOException::class)
     fun init() {
         path.normalize()
         path.createParentDirectories()
-        if (!path.toFile().exists()) {
-            path.createFile()
-        }
+        if (!path.toFile().exists()) path.createFile()
         configFile = YamlConfiguration.loadConfiguration(path.toFile())
     }
 
-    fun get(): FileConfiguration {
-        return configFile
-    }
+    fun get(): FileConfiguration = configFile
 
     @Throws(IOException::class)
-    fun save() {
-        configFile.save(path.toFile())
-    }
+    fun save() = configFile.save(path.toFile())
 
     fun reload() {
         configFile = YamlConfiguration.loadConfiguration(path.toFile())
