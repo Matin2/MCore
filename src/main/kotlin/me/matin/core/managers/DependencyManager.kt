7d4d8f6abeb0c7603c976254eb9b2cdcc73aef27
Matin <65161829+Matin2@users.noreply.github.com>
2024-05-09 16:01:44 +0330

    Update some features

--- src/main/kotlin/me/matin/core/managers/DependencyManager.kt
+++ src/main/kotlin/me/matin/core/managers/DependencyManager.kt
@@ -3,55 +3,46 @@ package me.matin.core.managers
 import org.bukkit.Bukkit
 import org.bukkit.plugin.Plugin
 
-@Suppress("UnstableApiUsage")
-class DependencyManager {
+@Suppress("UnstableApiUsage", "unused")
+object DependencyManager {
 
-    companion object {
+    @JvmStatic
+    fun isPluginInstalled(pluginName: String): Boolean {
+        val plugin = Bukkit.getPluginManager().getPlugin(pluginName)
+        return plugin != null && plugin.isEnabled
+    }
 
-        @JvmStatic
-        fun isPluginInstalled(pluginName: String): Boolean {
-            val plugin = Bukkit.getPluginManager().getPlugin(pluginName)
-            return plugin != null && plugin.isEnabled
-        }
-
-        @JvmStatic
-        fun arePluginsInstalled(pluginNames: Array<String>): Boolean {
-            val statues: MutableList<Boolean> = ArrayList()
-            for (pluginName in pluginNames) {
-                val plugin = Bukkit.getPluginManager().getPlugin(pluginName.trim())
-                if (plugin != null && plugin.isEnabled) {
-                    statues.add(true)
-                } else {
-                    statues.add(false)
-                }
-            }
-            for (b in statues) if (!b) return false
-            return true
-        }
-
-        @JvmStatic
-        fun getPlugin(pluginName: String): Plugin? {
-            return Bukkit.getPluginManager().getPlugin(pluginName)
-        }
-
-        @JvmStatic
-        fun checkDepends(plugin: Plugin, depends: Array<String>) {
-            for (pluginName in depends) {
-                if (!isPluginInstalled(pluginName)) {
-                    plugin.logger.warning("$pluginName is required but not installed!")
-                    Bukkit.getPluginManager().disablePlugin(plugin)
-                }
+    @JvmStatic
+    fun arePluginsInstalled(pluginNames: Array<String>): Boolean {
+        val statues: MutableList<Boolean> = ArrayList()
+        for (pluginName in pluginNames) {
+            val plugin = Bukkit.getPluginManager().getPlugin(pluginName.trim())
+            if (plugin != null && plugin.isEnabled) {
+                statues.add(true)
+            } else {
+                statues.add(false)
             }
         }
+        for (b in statues) if (!b) return false
+        return true
+    }
 
-        @JvmStatic
-        fun checkDepends(plugin: Plugin) {
-            for (pluginName in plugin.pluginMeta.pluginDependencies) {
-                if (pluginName == null) continue
-                if (isPluginInstalled(pluginName)) continue
-                plugin.logger.warning("$pluginName is required but not installed!")
-                Bukkit.getPluginManager().disablePlugin(plugin)
-            }
+    @JvmStatic
+    fun checkDepends(plugin: Plugin, depends: Array<String>, warning: String = "{plugin} is required but not installed!") {
+        for (pluginName in depends) {
+            if (isPluginInstalled(pluginName)) continue
+            plugin.logger.warning(warning.replace("{plugin}", pluginName))
+            Bukkit.getPluginManager().disablePlugin(plugin)
+        }
+    }
+
+    @JvmStatic
+    fun checkDepends(plugin: Plugin, warning: String = "{plugin} is required but not installed!") {
+        for (pluginName in plugin.pluginMeta.pluginDependencies) {
+            if (pluginName == null) continue
+            if (isPluginInstalled(pluginName)) continue
+            plugin.logger.warning(warning.replace("{plugin}", pluginName))
+            Bukkit.getPluginManager().disablePlugin(plugin)
         }
     }
 }
--- src/main/kotlin/me/matin/core/managers/item/ItemManager.kt
+++ src/main/kotlin/me/matin/core/managers/item/ItemManager.kt
@@ -9,122 +9,103 @@ import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.Damageable
 
-class ItemManager {
+object ItemManager {
 
-    companion object {
-
-        @JvmStatic
-        fun modify(type: ItemModifyType, modificationType: ItemModificationType, modificationAmount: Int, player: Player, slots: ArrayList<Int>) {
-            for (slot in slots) {
-                when (type) {
-                    ItemModifyType.AMOUNT -> modifyAmount(modificationType, modificationAmount, player, slot)
-                    ItemModifyType.DURABILITY -> modifyDurability(modificationType, modificationAmount, player, slot)
-                }
-            }
-        }
-
-        @JvmStatic
-        fun modify(type: ItemModifyType, modificationType: ItemModificationType, modificationAmount: Int, player: Player, slot: Int) {
+    @JvmStatic
+    fun modify(type: ItemModifyType, modificationType: ItemModificationType, modificationAmount: Int, player: Player, slots: ArrayList<Int>) {
+        for (slot in slots) {
+            if (slot !in 0..40) continue
             when (type) {
                 ItemModifyType.AMOUNT -> modifyAmount(modificationType, modificationAmount, player, slot)
                 ItemModifyType.DURABILITY -> modifyDurability(modificationType, modificationAmount, player, slot)
             }
         }
+    }
 
-        private fun modifyAmount(modification: ItemModificationType, amount: Int, player: Player, slot: Int) {
-            var mod: Int
-            val item = player.inventory.getItem(slot) ?: return
-            val itemAmount = if (item.type == Material.AIR) 0 else item.amount
-            val itemMaxAmount = if (item.type == Material.AIR) 0 else item.maxStackSize
-            mod = when (modification) {
-                ItemModificationType.SET -> amount
-                ItemModificationType.ADD -> itemAmount + amount
-                ItemModificationType.TAKE -> itemAmount - amount
-            }
-            if (mod > itemMaxAmount) mod = itemMaxAmount
-            if (itemAmount != 0) {
-                item.amount = mod
+    @JvmStatic
+    fun modify(type: ItemModifyType, modificationType: ItemModificationType, modificationAmount: Int, player: Player, slot: Int) {
+        when (type) {
+            ItemModifyType.AMOUNT -> modifyAmount(modificationType, modificationAmount, player, slot)
+            ItemModifyType.DURABILITY -> modifyDurability(modificationType, modificationAmount, player, slot)
+        }
+    }
+
+    private fun modifyAmount(modification: ItemModificationType, amount: Int, player: Player, slot: Int) {
+        var mod: Int
+        val item = player.inventory.getItem(slot) ?: return
+        if (item.type == Material.AIR) return
+        val itemAmount = item.amount
+        val itemMaxAmount = item.maxStackSize
+        mod = when (modification) {
+            ItemModificationType.SET -> amount
+            ItemModificationType.ADD -> itemAmount + amount
+            ItemModificationType.TAKE -> itemAmount - amount
+        }
+        if (mod > itemMaxAmount) mod = itemMaxAmount
+        if (itemAmount != 0) {
+            item.amount = mod
+        }
+    }
+
+    private fun modifyDurability(modification: ItemModificationType, amount: Int, player: Player, slot: Int) {
+        val item = player.inventory.getItem(slot) ?: return
+        if (item.type == Material.AIR || item.itemMeta.isUnbreakable) return
+        val damageable = item.itemMeta as? Damageable ?: return
+        val itemMaxDurability = item.type.maxDurability.toInt()
+        val mod: Int
+        val itemDamage = if (damageable.hasDamage()) damageable.damage else 0
+        mod = when (modification) {
+            ItemModificationType.SET -> itemMaxDurability - amount
+            ItemModificationType.ADD -> itemDamage - amount
+            ItemModificationType.TAKE -> itemDamage + amount
+        }
+        if (mod == 0 || item.type == Material.AIR) return
+        damageable.damage = mod
+        Bukkit.getScheduler().runTaskAsynchronously(Bukkit.getPluginManager().getPlugin("MCore")!!, Runnable {
+            item.setItemMeta(
+                damageable
+            )
+        })
+    }
+
+    @JvmStatic
+    fun drop(item: ItemStack, x: Int, y: Int, z: Int, world: World, blockFace: BlockFace) {
+        if (item.type == Material.AIR || item.amount == 0) return
+        world.dropItemNaturally(world.getBlockAt(x, y, z).getRelative(blockFace).location, item)
+    }
+
+    @JvmStatic
+    fun drop(item: ItemStack, location: Location, blockFace: BlockFace) {
+        if (item.type == Material.AIR || item.amount == 0) return
+        location.world.dropItemNaturally(location.block.getRelative(blockFace).location, item)
+    }
+
+    @JvmStatic
+    fun drop(item: ItemStack, x: Int, y: Int, z: Int, world: World) {
+        if (item.type == Material.AIR || item.amount == 0) return
+        world.dropItemNaturally(world.getBlockAt(x, y, z).location, item)
+    }
+
+    @JvmStatic
+    fun drop(item: ItemStack, location: Location) {
+        if (item.type == Material.AIR || item.amount == 0) return
+        location.world.dropItemNaturally(location, item)
+    }
+
+    @JvmStatic
+    fun getSlots(player: Player, slots: String): ArrayList<Int> {
+        val result = ArrayList<Int>()
+        slots.split(',').filter { it.isNotBlank() }.forEach {
+            when (it) {
+                "mainhand" -> result.add(player.inventory.heldItemSlot)
+                "offhand" -> result.add(40)
+                "helmet" -> result.add(36)
+                "chestplate" -> result.add(37)
+                "leggings" -> result.add(38)
+                "boots" -> result.add(39)
+                else -> it.toIntOrNull()?.let { slot -> result.add(slot) }
             }
         }
-
-        private fun modifyDurability(modification: ItemModificationType, amount: Int, player: Player, slot: Int) {
-            val item = player.inventory.getItem(slot) ?: return
-            if (item.type != Material.AIR && !item.itemMeta.isUnbreakable && item.itemMeta is Damageable) {
-                val damageable = item.itemMeta as Damageable
-                val itemMaxDurability = item.type.maxDurability.toInt()
-                val mod: Int
-                val itemDamage = if (damageable.hasDamage()) damageable.damage else 0
-                mod = when (modification) {
-                    ItemModificationType.SET -> itemMaxDurability - amount
-                    ItemModificationType.ADD -> itemDamage - amount
-                    ItemModificationType.TAKE -> itemDamage + amount
-                }
-                if (mod != 0 && item.type != Material.AIR) {
-                    damageable.damage = mod
-                    Bukkit.getScheduler().runTaskAsynchronously(Bukkit.getPluginManager().getPlugin("MCore")!!, Runnable {
-                        item.setItemMeta(
-                            damageable
-                        )
-                    })
-                }
-            }
-        }
-
-        @JvmStatic
-        fun drop(item: ItemStack, x: Int, y: Int, z: Int, world: World, blockFace: BlockFace) {
-            if (item.type != Material.AIR && item.amount != 0) {
-                val block = world.getBlockAt(x, y, z)
-                world.dropItemNaturally(block.getRelative(blockFace).location, item)
-            }
-        }
-
-        @JvmStatic
-        fun drop(item: ItemStack, location: Location, blockFace: BlockFace) {
-            if (item.type != Material.AIR && item.amount != 0) {
-                val block = location.block
-                location.world.dropItemNaturally(block.getRelative(blockFace).location, item)
-            }
-        }
-
-        @JvmStatic
-        fun drop(item: ItemStack, x: Int, y: Int, z: Int, world: World) {
-            if (item.type != Material.AIR && item.amount != 0) {
-                val block = world.getBlockAt(x, y, z)
-                world.dropItemNaturally(block.location, item)
-            }
-        }
-
-        @JvmStatic
-        fun drop(item: ItemStack, location: Location) {
-            if (item.type != Material.AIR && item.amount != 0) {
-                location.world.dropItemNaturally(location, item)
-            }
-        }
-
-        @JvmStatic
-        fun getSlots(player: Player, slots: String): ArrayList<Int> {
-            val slotsArray = slots.split(',').dropLastWhile { it.isEmpty() }.toTypedArray()
-            val result: ArrayList<Int> = ArrayList()
-            if (slotsArray.isNotEmpty()) {
-                for (slot in slotsArray) {
-                    val presetSlot = getPresetSlot(player, slot) ?: continue
-                    result.add(presetSlot)
-                }
-            }
-            return result
-        }
-
-        private fun getPresetSlot(p: Player, slot: String): Int? {
-            var result: Int? = slot.toIntOrNull()
-            when (slot) {
-                "mainhand", "main" -> result = p.inventory.heldItemSlot
-                "offhand", "off" -> result = 40
-                "helmet", "helm" -> result = 39
-                "chestplate", "chest" -> result = 38
-                "leggings", "leggs" -> result = 37
-                "boots" -> result = 36
-            }
-            return result
-        }
+        return result
     }
 }
