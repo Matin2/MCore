8281215018b352e3c041056386181e772e18a623
Matin <65161829+Matin2@users.noreply.github.com>
2024-05-26 18:07:32 +0330

    Add Coroutines and Reflections APIs

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,10 @@ dependencies {
 //    implementation("dev.jorel:commandapi-bukkit-shade:9.5.0")
     implementation("com.github.retrooper.packetevents:spigot:2.3.0")
 
-    api(kotlin("stdlib"))
-    api(kotlin("reflect"))
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("org.reflections:reflections:0.10.2")
 }
 
 tasks.shadowJar {
@@ -34,10 +36,16 @@ tasks.shadowJar {
     relocate("assets", "$dir.packetevents.assets")
     relocate("com.github.retrooper.packetevents", "$dir.packetevents.api")
     relocate("io.github.retrooper.packetevents", "$dir.packetevents.impl")
+    relocate("javassist", "$dir.reflections.javassist")
+    relocate("javax", "$dir.reflections.javax")
+    relocate("org.reflections", "$dir.reflections")
+    relocate("kotlinx.coroutines", "$dir.coroutines")
+    relocate("_COROUTINE", "$dir.coroutines.coroutine")
     dependencies {
         exclude(dependency("org.jetbrains:annotations"))
         exclude(dependency("com.google.code.gson:gson"))
         exclude(dependency("net.kyori:"))
+        exclude(dependency("org.slf4j:"))
     }
     archiveFileName.set("${project.name}-${project.version}.jar")
 }
