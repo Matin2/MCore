8bb2ea38945c50ea41e70486848fba72bee3852c
Matin <65161829+Matin2@users.noreply.github.com>
2024-07-25 14:05:59 +0330

    Add time to infos

--- src/main/kotlin/me/matin/core/Core.kt
+++ src/main/kotlin/me/matin/core/Core.kt
@@ -5,31 +5,34 @@ import de.tr7zw.changeme.nbtapi.NBTContainer
 import dev.jorel.commandapi.CommandAPI
 import dev.jorel.commandapi.CommandAPIBukkitConfig
 import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder
+import me.matin.core.managers.TextManager.toReadableString
 import me.matin.core.managers.dependency.DependencyListener
 import me.matin.core.managers.menu.MenuManager
 import org.bukkit.Bukkit
 import org.bukkit.World
 import org.bukkit.plugin.java.JavaPlugin
+import kotlin.time.measureTime
 
 class Core: JavaPlugin() {
     companion object {
+
         @JvmStatic
         lateinit var plugin: Core
+
         @JvmStatic
         var corePlayerTrackingRange: MutableMap<World, Int> = HashMap()
     }
 
-    override fun onEnable() {
+    override fun onEnable() = measureTime {
         plugin = this
         CommandAPI.onEnable()
         PacketEvents.getAPI().init()
         setPlayerTrackingRange(corePlayerTrackingRange)
         server.pluginManager.registerEvents(MenuManager, this)
         server.pluginManager.registerEvents(DependencyListener, this)
-        logger.info("Plugin enabled.")
-    }
+    }.let { logger.info("Plugin enabled in ${it.toReadableString(daySuffix = "day" to "days")}.") }
 
-    override fun onLoad() {
+    override fun onLoad() = measureTime {
         PacketEvents.setAPI(SpigotPacketEventsBuilder.build(this))
         PacketEvents.getAPI()?.also {
             it.settings
@@ -37,23 +40,22 @@ class Core: JavaPlugin() {
                 .checkForUpdates(false)
             it.load()
         }
-        CommandAPI.onLoad(CommandAPIBukkitConfig(this)
-            .shouldHookPaperReload(true)
-            .silentLogs(true)
-            .usePluginNamespace()
-            .initializeNBTAPI(NBTContainer::class.java, ::NBTContainer)
+        CommandAPI.onLoad(
+            CommandAPIBukkitConfig(this)
+                .shouldHookPaperReload(true)
+                .silentLogs(true)
+                .usePluginNamespace()
+                .initializeNBTAPI(NBTContainer::class.java, ::NBTContainer)
         )
-        logger.info("Plugin loaded.")
-    }
+    }.let { logger.info("Plugin loaded in ${it.toReadableString(daySuffix = "day" to "days")}.") }
 
-    override fun onDisable() {
+    override fun onDisable() = measureTime {
         Bukkit.getOnlinePlayers().forEach {
             MenuManager.checkCursor(it)
         }
         CommandAPI.onDisable()
         PacketEvents.getAPI().terminate()
-        logger.info("Plugin disabled.")
-    }
+    }.let { logger.info("Plugin disabled in ${it.toReadableString(daySuffix = "day" to "days")}.") }
 
     private fun setPlayerTrackingRange(playerTrackingRange: MutableMap<World, Int>) {
         playerTrackingRange.clear()
@@ -64,5 +66,4 @@ class Core: JavaPlugin() {
                 .config.getInt("world-settings.${it.name}.entity-tracking-range.players", defaultRange)
         }
     }
-
 }
