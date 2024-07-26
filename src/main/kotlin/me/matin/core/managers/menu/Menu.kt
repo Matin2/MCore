93fd6c8a94e23075409fda24bf6aced67461537a
Matin <65161829+Matin2@users.noreply.github.com>
2024-07-26 13:52:28 +0330

    Made open function async Convert tasksToRun to list Other Internal Changes

--- src/main/kotlin/me/matin/core/managers/menu/Menu.kt
+++ src/main/kotlin/me/matin/core/managers/menu/Menu.kt
@@ -1,8 +1,7 @@
 package me.matin.core.managers.menu
 
-import me.matin.core.Core
+import me.matin.core.managers.TaskManager
 import me.matin.core.managers.menu.items.MenuItem
-import me.matin.core.managers.menu.MenuItem as MenuItemAnnotation
 import me.matin.core.managers.menu.items.button.ButtonManager
 import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
@@ -11,6 +10,8 @@ import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.InventoryHolder
 import org.bukkit.scheduler.BukkitTask
 import kotlin.reflect.full.hasAnnotation
+import kotlin.time.Duration
+import me.matin.core.managers.menu.MenuItem as MenuItemAnnotation
 
 @Suppress("unused")
 abstract class Menu(private val player: Player): InventoryHolder {
@@ -23,9 +24,9 @@ abstract class Menu(private val player: Player): InventoryHolder {
     open val preventCursorLoss: Boolean = true
     private var opened: Boolean = false
     private val runningTasks = mutableSetOf<BukkitTask>()
-    private val tasksToRun: MutableSet<Triple<Pair<Long, Long>, Boolean, () -> Unit>> = mutableSetOf()
+    private val tasksToRun: MutableList<Triple<Pair<Duration, Duration>, Boolean, () -> Unit>> = mutableListOf()
 
-    fun open() {
+    fun open() = TaskManager.runTask(true) {
         type.type?.also {
             inventory = Bukkit.createInventory(this, it, title)
         } ?: run {
@@ -33,43 +34,52 @@ abstract class Menu(private val player: Player): InventoryHolder {
         }
         processItems()
         ButtonManager(this).manageDisplay()
-        player.openInventory(inventory)
-        opened = true
-        for ((intervalDelay, async, action) in tasksToRun) {
-            val task = Bukkit.getScheduler().run {
-                if (async) runTaskTimerAsynchronously(Core.plugin, action, intervalDelay.first, intervalDelay.second)
-                else runTaskTimer(Core.plugin, action, intervalDelay.first, intervalDelay.second)
-            }
+        TaskManager.runTask {
+            player.openInventory(inventory)
+            opened = true
+        }
+        while (!opened) Thread.sleep(10)
+        for ((delayInterval, async, action) in tasksToRun) {
+            val task = TaskManager.scheduleTask(delayInterval.first, delayInterval.second, async, action)
             runningTasks.add(task)
         }
         tasksToRun.removeAll { true }
     }
 
     fun close(closeInventory: Boolean = true) {
-        opened = false
         if (closeInventory) player.closeInventory()
-        runningTasks.forEach {
-            it.cancel()
-            runningTasks.remove(it)
-        }
-    }
-
-    private fun processItems() = this::class.members.forEach { member ->
-        if (member.hasAnnotation<MenuItemAnnotation>()) {
-            if (member.parameters.size == 1) {
-                val result = member.call(this) as? MenuItem ?: return@forEach
-                items.add(result)
+        opened = false
+        TaskManager.runTask(true) {
+            runningTasks.forEach {
+                it.cancel()
+                runningTasks.remove(it)
             }
         }
     }
 
-    fun scheduleTask(delay: Long = 0, interval: Long = 0, async: Boolean, task: () -> Unit) {
+    private fun processItems() =
+        this::class.members.filter { it.hasAnnotation<MenuItemAnnotation>() && it.parameters.size == 1 }
+            .forEach { member ->
+                when (val result = member.call(this)) {
+                    is MenuItem -> items.add(result)
+                    is Iterable<*> -> result.mapTo(items) {
+                        if (it !is MenuItem) return@forEach
+                        it
+                    }
+                }
+            }
+
+    fun scheduleTask(
+        delay: Duration = Duration.ZERO,
+        interval: Duration = Duration.ZERO,
+        async: Boolean = false,
+        task: () -> Unit
+    ) {
         if (opened) {
             tasksToRun.add(Triple(delay to interval, async, task))
             return
         }
-        val bukkitTask = if (async) Bukkit.getScheduler().runTaskTimerAsynchronously(Core.plugin, task, delay, interval)
-        else Bukkit.getScheduler().runTaskTimer(Core.plugin, task, delay, interval)
+        val bukkitTask = TaskManager.scheduleTask(delay, interval, async, task)
         runningTasks.add(bukkitTask)
     }
 
