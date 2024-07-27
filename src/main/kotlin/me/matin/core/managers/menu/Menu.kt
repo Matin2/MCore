4c36475ca60617ef95e683b4af7e41ca6431061d
Matin <65161829+Matin2@users.noreply.github.com>
2024-07-27 10:18:01 +0330

    New MenuUtils Internal Changes

--- src/main/kotlin/me/matin/core/managers/menu/Menu.kt
+++ src/main/kotlin/me/matin/core/managers/menu/Menu.kt
@@ -1,30 +1,22 @@
 package me.matin.core.managers.menu
 
 import me.matin.core.managers.TaskManager
-import me.matin.core.managers.menu.items.MenuItem
+import me.matin.core.managers.menu.items.button.Button
 import me.matin.core.managers.menu.items.button.ButtonManager
-import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.inventory.Inventory
-import org.bukkit.inventory.InventoryHolder
-import org.bukkit.scheduler.BukkitTask
 import kotlin.reflect.full.hasAnnotation
-import kotlin.time.Duration
-import me.matin.core.managers.menu.MenuItem as MenuItemAnnotation
 
 @Suppress("unused")
-abstract class Menu(private val player: Player): InventoryHolder {
+abstract class Menu(private val player: Player): InventoryMenu {
 
     private lateinit var inventory: Inventory
-    abstract val title: Component
-    abstract val type: MenuType
-    val items = mutableSetOf<MenuItem>()
-    open val freezeBottomInv: Boolean = false
-    open val preventCursorLoss: Boolean = true
+    override val buttons = mutableSetOf<Button>()
+    override val freezeBottomInv: Boolean = false
+    override val preventCursorLoss: Boolean = true
     private var opened: Boolean = false
-    private val runningTasks = mutableSetOf<BukkitTask>()
-    private val tasksToRun: MutableList<Triple<Pair<Duration, Duration>, Boolean, () -> Unit>> = mutableListOf()
+    private val util = MenuUtils()
 
     fun open() = TaskManager.runTask(true) {
         type.type?.also {
@@ -39,50 +31,39 @@ abstract class Menu(private val player: Player): InventoryHolder {
             opened = true
         }
         while (!opened) Thread.sleep(10)
-        for ((delayInterval, async, action) in tasksToRun) {
-            val task = TaskManager.scheduleTask(delayInterval.first, delayInterval.second, async, action)
-            runningTasks.add(task)
-        }
-        tasksToRun.removeAll { true }
+        util.scheduleOnOpen()
     }
 
     fun close(closeInventory: Boolean = true) {
         if (closeInventory) player.closeInventory()
         opened = false
         TaskManager.runTask(true) {
-            runningTasks.forEach {
-                it.cancel()
-                runningTasks.remove(it)
-            }
+            util.removeTasks()
         }
     }
 
-    private fun processItems() =
-        this::class.members.filter { it.hasAnnotation<MenuItemAnnotation>() && it.parameters.size == 1 }
+    private fun processItems() {
+        this::class.members.filter { it.hasAnnotation<MenuItem>() && it.parameters.size == 1 }
             .forEach { member ->
                 when (val result = member.call(this)) {
-                    is MenuItem -> items.add(result)
-                    is Iterable<*> -> result.mapTo(items) {
-                        if (it !is MenuItem) return@forEach
-                        it
+                    is Button -> buttons.add(result)
+                    is Iterable<*> -> {
+                        result.forEach {
+                            when (it) {
+                                is Button -> buttons.add(it)
+                            }
+                        }
                     }
                 }
             }
-
-    fun scheduleTask(
-        delay: Duration = Duration.ZERO,
-        interval: Duration = Duration.ZERO,
-        async: Boolean = false,
-        task: () -> Unit
-    ) {
-        if (opened) {
-            tasksToRun.add(Triple(delay to interval, async, task))
-            return
-        }
-        val bukkitTask = TaskManager.scheduleTask(delay, interval, async, task)
-        runningTasks.add(bukkitTask)
     }
 
+    //    fun scheduleTask(
+//        delay: Duration = Duration.ZERO,
+//        interval: Duration = Duration.ZERO,
+//        async: Boolean = false,
+//        task: () -> Unit
+//    ) = util.scheduleTask(delay, interval, async, task)
     override fun getInventory(): Inventory {
         return inventory
     }
---
+++ src/main/kotlin/me/matin/core/managers/menu/MenuUtils.kt
@@ -0,0 +1,43 @@
+package me.matin.core.managers.menu
+
+import me.matin.core.managers.TaskManager
+import org.bukkit.scheduler.BukkitTask
+import kotlin.time.Duration
+
+class MenuUtils {
+
+    private val runningTasks = mutableSetOf<BukkitTask>()
+    private val tasksToRun: MutableList<Triple<Pair<Duration, Duration>, Boolean, () -> Unit>> = mutableListOf()
+    private var open = false
+
+    fun scheduleOnOpen() {
+        open = true
+        for ((delayInterval, async, action) in tasksToRun) {
+            val task = TaskManager.scheduleTask(delayInterval.first, delayInterval.second, async, action)
+            runningTasks.add(task)
+        }
+        tasksToRun.removeAll { true }
+    }
+
+    fun removeTasks() {
+        open = false
+        runningTasks.forEach {
+            it.cancel()
+            runningTasks.remove(it)
+        }
+    }
+
+    fun scheduleTask(
+        delay: Duration = Duration.ZERO,
+        interval: Duration = Duration.ZERO,
+        async: Boolean = false,
+        task: () -> Unit
+    ) {
+        if (open) {
+            tasksToRun.add(Triple(delay to interval, async, task))
+            return
+        }
+        val bukkitTask = TaskManager.scheduleTask(delay, interval, async, task)
+        runningTasks.add(bukkitTask)
+    }
+}
