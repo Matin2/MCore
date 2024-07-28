1ab29209fbd0f46cf1704dfe85478d7589219fc9
Matin <65161829+Matin2@users.noreply.github.com>
2024-07-28 09:11:40 +0330

    Remove pages

--- src/main/kotlin/me/matin/core/managers/menu/items/MenuItem.kt
+++ src/main/kotlin/me/matin/core/managers/menu/items/MenuItem.kt
@@ -1,4 +1,4 @@
 package me.matin.core.managers.menu.items
 
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY)
-annotation class MenuItem(vararg val pages: Int = [])
+annotation class MenuItem
