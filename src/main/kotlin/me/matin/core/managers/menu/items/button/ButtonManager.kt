df8239da1b86beeae1cd2c683653f329f179a632
Matin <65161829+Matin2@users.noreply.github.com>
2024-07-28 14:32:02 +0330

    Support changes in ListMenu

--- src/main/kotlin/me/matin/core/managers/menu/items/button/ButtonManager.kt
+++ src/main/kotlin/me/matin/core/managers/menu/items/button/ButtonManager.kt
@@ -1,8 +1,6 @@
 package me.matin.core.managers.menu.items.button
 
 import me.matin.core.managers.menu.InventoryMenu
-import me.matin.core.managers.menu.menus.ListMenu
-import me.matin.core.managers.menu.menus.Menu
 import org.bukkit.event.inventory.InventoryClickEvent
 
 class ButtonManager(private val menu: InventoryMenu) {
@@ -15,19 +13,12 @@ class ButtonManager(private val menu: InventoryMenu) {
     }
 
     fun manageDisplay() {
-        when (menu) {
-            is Menu -> menu.buttons.forEach { display(it) }
-            is ListMenu<*> -> menu.buttonsMap.filter { (_, pages) ->
-                pages == null || menu.page !in pages
-            }.forEach { display(it.key) }
-        }
-    }
-
-    private fun display(button: Button) {
-        if (!button.show) return
-        button.menu = menu
-        button.slots.forEach {
-            menu.inventory.setItem(it, button.statesDisplay[button.state].toItem())
+        menu.buttons.forEach { button ->
+            if (!button.show) return
+            button.menu = menu
+            button.slots.forEach {
+                menu.inventory.setItem(it, button.statesDisplay[button.state].toItem())
+            }
         }
     }
 }
