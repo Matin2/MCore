701d0c893dfd1f3a6370d38ce165d2397a343e14
Matin <65161829+Matin2@users.noreply.github.com>
2024-07-29 18:24:05 +0330

    Move buttons to ButtonManager

--- src/main/kotlin/me/matin/core/managers/menu/InventoryMenu.kt
+++ src/main/kotlin/me/matin/core/managers/menu/InventoryMenu.kt
@@ -1,15 +1,16 @@
 package me.matin.core.managers.menu
 
-import me.matin.core.managers.menu.items.Filler
-import me.matin.core.managers.menu.items.button.Button
+import me.matin.core.managers.menu.items.other.Filler
 import net.kyori.adventure.text.Component
+import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.inventory.InventoryHolder
 
 abstract class InventoryMenu: InventoryHolder {
 
     abstract val title: Component
     abstract val type: MenuType
-    open val buttons: MutableSet<Button> = mutableSetOf()
+    abstract fun manageBehaviour(event: InventoryClickEvent)
+    abstract fun close(closeInventory: Boolean = true)
     open val filler: Filler = Filler()
     open val freezeBottomInv: Boolean = false
     open val preventCursorLoss: Boolean = true
