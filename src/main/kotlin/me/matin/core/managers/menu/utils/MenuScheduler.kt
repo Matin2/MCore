14869c44d9b49be7cb5fb420a7b527b98fa25a8f
Matin <65161829+Matin2@users.noreply.github.com>
2024-08-01 11:22:27 +0330

    Remove close and rename methods

--- src/main/kotlin/me/matin/core/managers/menu/utils/MenuScheduler.kt
+++ src/main/kotlin/me/matin/core/managers/menu/utils/MenuScheduler.kt
@@ -1,9 +1,6 @@
 package me.matin.core.managers.menu.utils
 
 import me.matin.core.Core
-import me.matin.core.managers.menu.items.slot.ItemDeleteReason
-import me.matin.core.managers.menu.items.slot.SlotManager
-import org.bukkit.entity.Player
 import org.bukkit.scheduler.BukkitTask
 import kotlin.time.Duration
 
@@ -13,22 +10,8 @@ class MenuUtils {
     private val tasksToRun: MutableList<Triple<Boolean, Pair<Duration, Duration>, () -> Unit>> = mutableListOf()
     var open = false
 
-    fun close(closeInventory: Boolean, slotManager: SlotManager, player: Player) {
-        open = false
-        slotManager.slots.forEach { slot ->
-            slot.item?.also {
-                slot.itemDeleteAction?.invoke(it, ItemDeleteReason.MENU_CLOSED) ?: run {
-                    player.inventory.addItem(it)
-                }
-            }
-        }
-        if (closeInventory) player.closeInventory()
-        Core.scheduleTask(true) {
-            removeTasks()
-        }
-    }
-
-    fun scheduleOnOpen() {
+    fun onOpen() {
+        open = true
         for ((async, delayInterval, action) in tasksToRun) {
             val task = Core.scheduleTask(async, delayInterval.first, delayInterval.second, action)
             runningTasks.add(task)
@@ -36,16 +19,15 @@ class MenuUtils {
         tasksToRun.clear()
     }
 
-    private fun removeTasks() {
+    fun onClose() {
+        open = false
         runningTasks.forEach {
             it.cancel()
             runningTasks.remove(it)
         }
     }
 
-    fun scheduleTask(
-        async: Boolean = false, delay: Duration = Duration.ZERO, interval: Duration = Duration.ZERO, task: () -> Unit
-    ) {
+    fun schedule(async: Boolean, delay: Duration, interval: Duration, task: () -> Unit) {
         if (open) {
             tasksToRun.add(Triple(async, delay to interval, task))
             return
