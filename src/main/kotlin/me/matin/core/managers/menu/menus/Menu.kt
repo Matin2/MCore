889fc5e4b3d469dbd6d7debde66f4a6a28aee53e
Matin <65161829+Matin2@users.noreply.github.com>
2024-08-01 11:53:59 +0330

    Add processItems

--- src/main/kotlin/me/matin/core/managers/menu/menus/Menu.kt
+++ src/main/kotlin/me/matin/core/managers/menu/menus/Menu.kt
@@ -3,9 +3,13 @@ package me.matin.core.managers.menu.menus
 import me.matin.core.Core
 import me.matin.core.managers.menu.MenuType
 import me.matin.core.managers.menu.handlers.MenuHandler
+import me.matin.core.managers.menu.items.MenuItem
+import me.matin.core.managers.menu.items.button.Button
 import me.matin.core.managers.menu.items.other.Filler
+import me.matin.core.managers.menu.items.slot.Slot
 import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
+import kotlin.reflect.full.hasAnnotation
 import kotlin.time.Duration
 
 @Suppress("unused")
@@ -21,6 +25,7 @@ abstract class Menu {
 
     fun open() {
         Core.scheduleTask(true) {
+            processItems()
             handler.open()
         }
     }
@@ -35,4 +40,21 @@ abstract class Menu {
     ) = handler.scheduleTask(async, delay, interval, task)
 
     fun updateItems() = handler.updateItems(false, (0..<handler.inventory.size).toMutableSet())
+
+    private fun processItems() {
+        this::class.members.filter { it.hasAnnotation<MenuItem>() && it.parameters.size == 1 }.forEach { member ->
+            when (val result = member.call(this)) {
+                is Button -> handler.addButton(result)
+                is Slot -> handler.addSlot(result)
+                is Iterable<*> -> {
+                    result.forEach {
+                        when (it) {
+                            is Button -> handler.addButton(it)
+                            is Slot -> handler.addSlot(it)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
