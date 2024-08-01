139faf639978693aa2eb75373d584a5a068fbbf1
Matin <65161829+Matin2@users.noreply.github.com>
2024-08-01 11:24:27 +0330

    New menu handlers

---
+++ src/main/kotlin/me/matin/core/managers/menu/handlers/ListMenuHandler.kt
@@ -0,0 +1,55 @@
+package me.matin.core.managers.menu.handlers
+
+import me.matin.core.Core
+import me.matin.core.managers.menu.items.button.ButtonManager
+import me.matin.core.managers.menu.items.other.Filler
+import me.matin.core.managers.menu.items.other.MenuList
+import me.matin.core.managers.menu.items.slot.SlotManager
+import me.matin.core.managers.menu.menus.ListMenu
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryInteractEvent
+
+@Suppress("MemberVisibilityCanBePrivate", "unused")
+class ListMenuHandler<T>(override val menu: ListMenu<T>): MenuHandler(menu) {
+
+    private lateinit var fillerSlots: Set<Int>
+    private val buttonManager: ButtonManager = ButtonManager()
+    private val slotManager: SlotManager = SlotManager()
+    private val fillerManager: Filler.Manager = Filler.Manager()
+    private var listManager = MenuList.Manager<T>()
+    var pages: Int = 0
+    var page: Int = menu.page
+        set(value) {
+            pages = (menu.list.list.size / menu.list.slots.size) + 1
+            field = when {
+                value < 0 -> (value % pages) + pages
+                value >= pages -> value % pages
+                else -> value
+            }
+            Core.scheduleTask(true) { privateUpdateItems() }
+        }
+
+    override fun open() {
+        listManager.makeListMap(menu.list)
+        super.open()
+    }
+
+    override fun manageBehavior(event: InventoryInteractEvent) {
+        super.manageBehavior(event)
+        listManager.manageBehavior(event as? InventoryClickEvent ?: return, menu.list, page)
+    }
+
+    private fun privateUpdateItems() {
+        listManager.manageDisplay(inventory, menu.list, page)
+        val fillerSlots = (0..<inventory.size).toMutableSet()
+        fillerSlots.removeAll(menu.list.slots)
+        super.updateItems(false, fillerSlots)
+    }
+
+    override fun updateItems(useDefaultItem: Boolean, fillerSlots: MutableSet<Int>) {
+        pages = (menu.list.list.size / menu.list.slots.size) + 1
+        listManager.manageDisplay(inventory, menu.list, page)
+        fillerSlots.removeAll(menu.list.slots)
+        super.updateItems(useDefaultItem, fillerSlots)
+    }
+}
---
+++ src/main/kotlin/me/matin/core/managers/menu/handlers/MenuHandler.kt
@@ -0,0 +1,106 @@
+package me.matin.core.managers.menu.handlers
+
+import me.matin.core.Core
+import me.matin.core.managers.menu.items.MenuItem
+import me.matin.core.managers.menu.items.button.Button
+import me.matin.core.managers.menu.items.button.ButtonManager
+import me.matin.core.managers.menu.items.other.Filler
+import me.matin.core.managers.menu.items.slot.ItemDeleteReason
+import me.matin.core.managers.menu.items.slot.Slot
+import me.matin.core.managers.menu.items.slot.SlotManager
+import me.matin.core.managers.menu.menus.Menu
+import me.matin.core.managers.menu.utils.MenuScheduler
+import org.bukkit.Bukkit
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryInteractEvent
+import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.InventoryHolder
+import kotlin.reflect.full.hasAnnotation
+import kotlin.time.Duration
+
+@Suppress("unused")
+open class MenuHandler(open val menu: Menu): InventoryHolder {
+
+    private lateinit var inventory: Inventory
+    private val buttonManager: ButtonManager = ButtonManager()
+    private val slotManager: SlotManager = SlotManager()
+    private val fillerManager: Filler.Manager = Filler.Manager()
+    private val scheduler = MenuScheduler()
+
+    open fun open() {
+        createInventory()
+        processItems()
+        updateItems(true, (0..<inventory.size).toMutableSet())
+        var open = false
+        Core.scheduleTask {
+            menu.player.openInventory(inventory)
+            open = true
+        }
+        while (!open) Thread.sleep(10)
+        scheduler.onOpen()
+    }
+
+    private fun createInventory() {
+        inventory = menu.type.type?.let { Bukkit.createInventory(this, it, menu.title) } ?: let {
+            Bukkit.createInventory(this, maxOf(minOf(menu.type.rows!!, 6), 1) * 9, menu.title)
+        }
+    }
+
+    fun close(closeInventory: Boolean = true) {
+        if (closeInventory) menu.player.closeInventory()
+        Core.scheduleTask(true) {
+            scheduler.onClose()
+            slotManager.slots.forEach { slot ->
+                val item = slot.item ?: return@forEach
+                slot.itemDeleteAction?.also { action ->
+                    Core.scheduleTask { action(item, ItemDeleteReason.MENU_CLOSED) }
+                } ?: run { menu.player.inventory.addItem(item) }
+            }
+        }
+    }
+
+    fun scheduleTask(async: Boolean, delay: Duration, interval: Duration, task: () -> Unit) {
+        scheduler.schedule(async, delay, interval, task)
+    }
+
+    fun publicUpdateItems() {
+        updateItems(false, (0..<inventory.size).toMutableSet())
+    }
+
+    open fun manageBehavior(event: InventoryInteractEvent) {
+        slotManager.manageBehavior(event)
+        if (event !is InventoryClickEvent) return
+        buttonManager.manageBehavior(event)
+        fillerManager.manageBehavior(event, menu.filler)
+    }
+
+    open fun updateItems(useDefaultItem: Boolean, fillerSlots: MutableSet<Int>) {
+        buttonManager.manageDisplay(inventory, fillerSlots)
+        slotManager.manageDisplay(inventory, fillerSlots, useDefaultItem)
+        fillerManager.apply {
+            slots = fillerSlots
+            manageDisplay(inventory, menu.filler)
+        }
+    }
+
+    private fun processItems() {
+        this::class.members.filter { it.hasAnnotation<MenuItem>() && it.parameters.size == 1 }.forEach { member ->
+            when (val result = member.call(this)) {
+                is Button -> buttonManager.buttons.add(result)
+                is Slot -> slotManager.slots.add(result)
+                is Iterable<*> -> {
+                    result.forEach {
+                        when (it) {
+                            is Button -> buttonManager.buttons.add(it)
+                            is Slot -> slotManager.slots.add(it)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun getInventory(): Inventory {
+        return inventory
+    }
+}
