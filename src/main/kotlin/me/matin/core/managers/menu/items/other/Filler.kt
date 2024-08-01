cff9bd6d5fb2389fbe2b509aae72bac99f8c1afc
Matin <65161829+Matin2@users.noreply.github.com>
2024-08-01 11:19:53 +0330

    Move fillerSlots to fillerManager

--- src/main/kotlin/me/matin/core/managers/menu/items/other/Filler.kt
+++ src/main/kotlin/me/matin/core/managers/menu/items/other/Filler.kt
@@ -1,6 +1,5 @@
 package me.matin.core.managers.menu.items.other
 
-import me.matin.core.managers.menu.InventoryMenu
 import me.matin.core.managers.menu.items.button.ButtonAction
 import me.matin.core.managers.menu.utils.DisplayItem
 import me.matin.core.managers.menu.utils.Interacted
@@ -11,13 +10,15 @@ class Filler(val display: DisplayItem = DisplayItem(), val interactAction: Inter
 
     class Manager {
 
-        fun manageBehavior(event: InventoryClickEvent, filler: Filler, slots: Set<Int>) {
+        lateinit var slots: Set<Int>
+
+        fun manageBehavior(event: InventoryClickEvent, filler: Filler) {
             if (event.slot !in slots) return
             event.isCancelled = true
             filler.interactAction(Interacted(event, ButtonAction[event.click, event.hotbarButton] ?: return))
         }
 
-        fun manageDisplay(inventory: Inventory, filler: Filler, slots: Set<Int>) = slots.forEach {
+        fun manageDisplay(inventory: Inventory, filler: Filler) = slots.forEach {
             inventory.setItem(it, filler.display.toItem())
         }
     }
