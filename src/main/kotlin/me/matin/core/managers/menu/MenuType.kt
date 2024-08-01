2492c6a6ab3a13dd2d4636283042750744cda7dc
Matin <65161829+Matin2@users.noreply.github.com>
2024-08-01 11:23:42 +0330

    Remove unsupported types

--- src/main/kotlin/me/matin/core/managers/menu/MenuType.kt
+++ src/main/kotlin/me/matin/core/managers/menu/MenuType.kt
@@ -7,13 +7,9 @@ import org.jetbrains.annotations.Range
 sealed class MenuType private constructor(val type: InventoryType?, open val rows: Int?) {
 
     data class NORMAL(override val rows: @Range(from = 1, to = 6) Int): MenuType(null, rows)
-    data object ANVIL: MenuType(InventoryType.ANVIL, null)
-    data object BREWING: MenuType(InventoryType.BREWING, null)
     data object DISPENSER: MenuType(InventoryType.DISPENSER, null)
-    data object FURNACE: MenuType(InventoryType.FURNACE, null)
     data object GRINDSTONE: MenuType(InventoryType.GRINDSTONE, null)
     data object HOPPER: MenuType(InventoryType.HOPPER, null)
-    data object SMITHING: MenuType(InventoryType.SMITHING, null)
     data object WORKBENCH: MenuType(InventoryType.WORKBENCH, null)
 
     companion object {
