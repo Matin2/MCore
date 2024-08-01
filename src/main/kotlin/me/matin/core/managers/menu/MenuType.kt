8aeba3a0ea3678dcff084de173f4e4f4bfef7b73
Matin <65161829+Matin2@users.noreply.github.com>
2024-08-01 11:24:02 +0330

    Remove unused get method

--- src/main/kotlin/me/matin/core/managers/menu/MenuType.kt
+++ src/main/kotlin/me/matin/core/managers/menu/MenuType.kt
@@ -11,20 +11,4 @@ sealed class MenuType private constructor(val type: InventoryType?, open val row
     data object GRINDSTONE: MenuType(InventoryType.GRINDSTONE, null)
     data object HOPPER: MenuType(InventoryType.HOPPER, null)
     data object WORKBENCH: MenuType(InventoryType.WORKBENCH, null)
-
-    companion object {
-
-        @JvmStatic
-        infix operator fun get(type: InventoryType): MenuType = when (type) {
-            InventoryType.ANVIL -> ANVIL
-            InventoryType.BREWING -> BREWING
-            InventoryType.DISPENSER -> DISPENSER
-            InventoryType.FURNACE -> FURNACE
-            InventoryType.GRINDSTONE -> GRINDSTONE
-            InventoryType.HOPPER -> HOPPER
-            InventoryType.SMITHING -> SMITHING
-            InventoryType.WORKBENCH -> WORKBENCH
-            else -> NORMAL(3)
-        }
-    }
 }
