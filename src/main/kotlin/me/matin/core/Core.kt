ad4e8c519679b6ff045e1c88f55a817522611ed8
Matin <65161829+Matin2@users.noreply.github.com>
2024-08-16 15:27:18 +0330

    Rework

--- src/main/kotlin/me/matin/core/Core.kt
+++ src/main/kotlin/me/matin/core/Core.kt
@@ -6,50 +6,19 @@ import dev.jorel.commandapi.CommandAPI
 import dev.jorel.commandapi.CommandAPIBukkitConfig
 import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder
 import me.arcaniax.hdb.api.HeadDatabaseAPI
-import me.matin.core.managers.TaskManager
-import me.matin.core.managers.TextManager.toReadableString
+import me.matin.core.managers.Extras.text
+import me.matin.core.managers.PacketManager
 import me.matin.core.managers.dependency.DependencyListener
 import me.matin.core.managers.dependency.PluginManager
 import me.matin.core.managers.menu.MenuListener
-import me.matin.core.managers.PacketManager
 import net.skinsrestorer.api.SkinsRestorer
 import net.skinsrestorer.api.SkinsRestorerProvider
 import org.bukkit.Bukkit
 import org.bukkit.World
 import org.bukkit.plugin.java.JavaPlugin
-import kotlin.time.Duration
 import kotlin.time.measureTime
 
 class Core: JavaPlugin() {
-
-    companion object {
-
-        @JvmStatic
-        lateinit var instance: Core
-
-        @JvmStatic
-        var skinsRestorer: SkinsRestorer? = null
-
-        @JvmStatic
-        var headDatabase: HeadDatabaseAPI? = null
-
-        @JvmStatic
-        var headDB = false
-
-        @JvmStatic
-        var corePlayerTrackingRange: MutableMap<World, Int> = HashMap()
-
-        @JvmStatic
-        val packetInvTitle = PacketManager.InventoryTitle()
-
-        @JvmStatic
-        fun scheduleTask(
-            async: Boolean = false,
-            delay: Duration = Duration.ZERO,
-            interval: Duration = Duration.ZERO,
-            task: () -> Unit
-        ) = TaskManager(instance).invoke(async, delay, interval, task)
-    }
 
     override fun onEnable() = measureTime {
         instance = this
@@ -59,14 +28,14 @@ class Core: JavaPlugin() {
             eventManager.registerListeners(packetInvTitle)
         }
         val softDepends = setOf("SkinsRestorer", "HeadDatabase", "HeadDB")
-        checkDepends(softDepends)
-        monitorDepends(softDepends)
+        depends = Depends(softDepends)
         setPlayerTrackingRange(corePlayerTrackingRange)
         server.pluginManager.apply {
             registerEvents(MenuListener, instance)
             registerEvents(DependencyListener, instance)
+            registerEvents(TestListener, instance)
         }
-    }.let { logger.info("Plugin enabled in ${it.toReadableString()}.") }
+    }.let { logger.info("Plugin enabled in ${it.text()}.") }
 
     override fun onLoad() = measureTime {
         PacketEvents.setAPI(SpigotPacketEventsBuilder.build(this))
@@ -83,23 +52,45 @@ class Core: JavaPlugin() {
                 .usePluginNamespace()
                 .initializeNBTAPI(NBTContainer::class.java, ::NBTContainer)
         )
-    }.let { logger.info("Plugin loaded in ${it.toReadableString()}.") }
+    }.let { logger.info("Plugin loaded in ${it.text()}.") }
 
     override fun onDisable() = measureTime {
         Bukkit.getScheduler().cancelTasks(this)
         Bukkit.getOnlinePlayers().forEach(MenuListener::checkCursor)
         CommandAPI.onDisable()
         PacketEvents.getAPI().terminate()
-    }.let { logger.info("Plugin disabled in ${it.toReadableString()}.") }
+    }.let { logger.info("Plugin disabled in ${it.text()}.") }
 
-    private fun checkDepends(softDepends: Set<String>) = PluginManager.checkState(softDepends) { installed, _ ->
-        if ("SkinsRestorer" in installed) skinsRestorer = SkinsRestorerProvider.get()
-        if ("HeadDatabase" in installed) headDatabase = HeadDatabaseAPI()
-        if ("HeadDB" in installed) headDB = true
+    private fun setPlayerTrackingRange(playerTrackingRange: MutableMap<World, Int>) {
+        playerTrackingRange.clear()
+        val defaultRange =
+            Bukkit.getServer().spigot().config.getInt("world-settings.default.entity-tracking-range.players", 64)
+        Bukkit.getServer().worlds.forEach {
+            playerTrackingRange[it] = Bukkit.getServer().spigot()
+                .config.getInt("world-settings.${it.name}.entity-tracking-range.players", defaultRange)
+        }
     }
 
-    private fun monitorDepends(softDepends: Set<String>) =
-        PluginManager.monitorState(softDepends.toSet()) { installed, missing ->
+    class Depends(depends: Set<String>) {
+
+        var skinsRestorer: SkinsRestorer? = null
+        var headDatabase: HeadDatabaseAPI? = null
+        var headDB: Boolean = false
+
+        init {
+            checkDepends(depends)
+            monitorDepends(depends)
+        }
+
+        private fun checkDepends(softDepends: Set<String>) = PluginManager.checkState(softDepends) { installed, _ ->
+            if ("SkinsRestorer" in installed) skinsRestorer = SkinsRestorerProvider.get()
+            if ("HeadDatabase" in installed) headDatabase = HeadDatabaseAPI()
+            if ("HeadDB" in installed) headDB = true
+        }
+
+        private fun monitorDepends(softDepends: Set<String>) = PluginManager.monitorState(
+            softDepends.toSet()
+        ) { installed, missing ->
             when ("SkinsRestorer") {
                 in installed -> skinsRestorer = SkinsRestorerProvider.get()
                 in missing -> skinsRestorer = null
@@ -113,14 +104,18 @@ class Core: JavaPlugin() {
                 in missing -> headDB = false
             }
         }
+    }
 
-    private fun setPlayerTrackingRange(playerTrackingRange: MutableMap<World, Int>) {
-        playerTrackingRange.clear()
-        val defaultRange =
-            Bukkit.getServer().spigot().config.getInt("world-settings.default.entity-tracking-range.players", 64)
-        Bukkit.getServer().worlds.forEach {
-            playerTrackingRange[it] = Bukkit.getServer().spigot()
-                .config.getInt("world-settings.${it.name}.entity-tracking-range.players", defaultRange)
-        }
+    companion object {
+
+        @JvmStatic
+        lateinit var instance: Core
+
+        @JvmStatic
+        lateinit var depends: Depends
+
+        @JvmStatic
+        var corePlayerTrackingRange: MutableMap<World, Int> = HashMap()
+        internal val packetInvTitle = PacketManager.InventoryTitle()
     }
 }
--- src/main/kotlin/me/matin/core/managers/TaskManager.kt
+++ src/main/kotlin/me/matin/core/managers/TaskManager.kt
@@ -1,39 +1,42 @@
 package me.matin.core.managers
 
-import me.matin.core.managers.TextManager.toTicks
+import me.matin.core.Core
+import me.matin.core.managers.Extras.ticks
 import org.bukkit.Bukkit
 import org.bukkit.plugin.Plugin
 import org.bukkit.scheduler.BukkitTask
 import kotlin.time.Duration
 
-class TaskManager(private val plugin: Plugin) {
+@Suppress("UnusedReceiverParameter")
+object TaskManager {
 
-    operator fun invoke(
+    @JvmStatic
+    fun Any.schedule(
+        plugin: Plugin,
         async: Boolean = false,
         delay: Duration = Duration.ZERO,
         interval: Duration = Duration.ZERO,
         task: () -> Unit
-    ): BukkitTask = when (async) {
-        true -> Bukkit.getScheduler().run {
-            when {
-                interval != Duration.ZERO -> runTaskTimerAsynchronously(
-                    plugin,
-                    task,
-                    delay.toTicks(),
-                    interval.toTicks()
-                )
-
-                delay != Duration.ZERO -> runTaskLaterAsynchronously(plugin, task, delay.toTicks())
+    ): BukkitTask = Bukkit.getScheduler().run {
+        when (async) {
+            true -> when {
+                interval != Duration.ZERO -> runTaskTimerAsynchronously(plugin, task, delay.ticks, interval.ticks)
+                delay != Duration.ZERO -> runTaskLaterAsynchronously(plugin, task, delay.ticks)
                 else -> runTaskAsynchronously(plugin, task)
             }
-        }
 
-        false -> Bukkit.getScheduler().run {
-            when {
-                interval != Duration.ZERO -> runTaskTimer(plugin, task, delay.toTicks(), interval.toTicks())
-                delay != Duration.ZERO -> runTaskLater(plugin, task, delay.toTicks())
+            false -> when {
+                interval != Duration.ZERO -> runTaskTimer(plugin, task, delay.ticks, interval.ticks)
+                delay != Duration.ZERO -> runTaskLater(plugin, task, delay.ticks)
                 else -> runTask(plugin, task)
             }
         }
     }
+
+    internal fun Any.schedule(
+        async: Boolean = false,
+        delay: Duration = Duration.ZERO,
+        interval: Duration = Duration.ZERO,
+        task: () -> Unit
+    ): BukkitTask = schedule(Core.instance, async, delay, interval, task)
 }
