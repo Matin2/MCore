afb9e1343c36b6b61469b3b6b33ec500245c6755
Matin <65161829+Matin2@users.noreply.github.com>
2024-08-16 15:42:57 +0330

    Rework drop method

--- src/main/kotlin/me/matin/core/managers/item/ItemManager.kt
+++ src/main/kotlin/me/matin/core/managers/item/ItemManager.kt
@@ -2,10 +2,10 @@ package me.matin.core.managers.item
 
 import org.bukkit.Location
 import org.bukkit.Material
-import org.bukkit.World
 import org.bukkit.block.BlockFace
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
+import java.util.*
 
 @Suppress("unused")
 object ItemManager {
@@ -13,28 +13,14 @@ object ItemManager {
     val ItemStack?.isAir get() = this?.takeUnless { it.amount == 0 || it.type == Material.AIR } == null
     val air get() = ItemStack(Material.AIR)
 
-    @JvmStatic
-    fun drop(item: ItemStack, x: Int, y: Int, z: Int, world: World, blockFace: BlockFace) {
-        if (item.type == Material.AIR || item.amount == 0) return
-        world.dropItemNaturally(world.getBlockAt(x, y, z).getRelative(blockFace).location, item)
-    }
 
     @JvmStatic
-    fun drop(item: ItemStack, location: Location, blockFace: BlockFace) {
-        if (item.type == Material.AIR || item.amount == 0) return
-        location.world.dropItemNaturally(location.block.getRelative(blockFace).location, item)
-    }
 
     @JvmStatic
-    fun drop(item: ItemStack, x: Int, y: Int, z: Int, world: World) {
-        if (item.type == Material.AIR || item.amount == 0) return
-        world.dropItemNaturally(world.getBlockAt(x, y, z).location, item)
-    }
-
-    @JvmStatic
-    fun drop(item: ItemStack, location: Location) {
-        if (item.type == Material.AIR || item.amount == 0) return
-        location.world.dropItemNaturally(location, item)
+    fun drop(item: ItemStack, location: Location, blockFace: Optional<BlockFace> = Optional.empty()) {
+        if (item.isEmpty) return
+        val dropLocation = if (blockFace.isPresent) location.block.getRelative(blockFace.get()).location else location
+        location.world.dropItemNaturally(dropLocation, item)
     }
 
     @JvmStatic
