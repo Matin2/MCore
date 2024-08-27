4cd9dfbf82a904e9728a7c4a0cdab748f291b8cd
Matin <65161829+Matin2@users.noreply.github.com>
2024-08-27 10:13:29 +0330

    New Mlib implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     compileOnly("net.skinsrestorer:skinsrestorer-api:15.4.2")
     compileOnly("com.arcaniax:HeadDatabase-API:1.3.2")
     compileOnly("com.github.TheSilentPro:HeadDB:5.0.0-rc.11")
+    implementation(fileTree("libs"))
     implementation("de.tr7zw:item-nbt-api:2.13.2")
     implementation("dev.jorel:commandapi-bukkit-shade:9.5.3")
     implementation("com.github.retrooper:packetevents-spigot:2.3.1-SNAPSHOT")
