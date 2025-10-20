a3ad6bf8f8056fa19f7757c9e4b4516672ce2ce4
Matin <65161829+Matin2@users.noreply.github.com>
2025-10-20 22:04:03 +0330

    Gradle Changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.internal.config.LanguageFeature
 
 plugins {
-	val kotlinVersion = "2.2.0"
-	kotlin("jvm") version kotlinVersion
-	kotlin("plugin.serialization") version kotlinVersion
-	id("io.github.goooler.shadow") version "8.1.8"
-	id("maven-publish")
+	alias(libs.plugins.kotlin.jvm)
+	alias(libs.plugins.kotlin.serialization)
+	alias(libs.plugins.shadow)
+	`version-catalog`
+	`maven-publish`
 	idea
 }
 
@@ -22,71 +22,64 @@ repositories {
 }
 
 dependencies {
-	compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
-	compileOnly("net.skinsrestorer:skinsrestorer-api:15.7.8")
-	compileOnly("com.arcaniax:HeadDatabase-API:1.3.2")
-	compileOnly("com.github.TheSilentPro:HeadDB:6.0.0-rc.2")
+	compileOnly(libs.paper)
+	compileOnly(libs.skinsrestorer)
 	
-	implementation(fileTree("libs"))
-	implementation("de.tr7zw:item-nbt-api:2.15.1")
-	implementation("com.github.retrooper:packetevents-spigot:2.9.4")
+	api(fileTree("libs"))
+	api(libs.nbtapi)
+	api(libs.packetevents)
 	
-	implementation(kotlin("stdlib"))
-	implementation(kotlin("reflect"))
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
-	
-	implementation("org.spongepowered:configurate-yaml:4.2.0")
-	implementation("org.spongepowered:configurate-extra-kotlin:4.2.0")
-	implementation("com.github.ItsDoot:configurate-serialization:0.1.1")
-}
-
-tasks.shadowJar {
-	val relocations = mapOf(
-		"me.matin.mlib" to "mlib",
-		"de.tr7zw.changeme.nbtapi" to "nbtapi",
-		//PacketEvents
-		"assets" to "packetevents.assets",
-		"com.github.retrooper.packetevents" to "packetevents.api",
-		"io.github.retrooper.packetevents" to "packetevents.impl",
-		//Configurate
-		"org.spongepowered.configurate" to "configurate",
-		"io.leangen.geantyref" to "configurate.geantyref",
-		"pw.dotdash.configurate" to "configurate",
-	)
-	val exclusions = setOf(
-		"org.jetbrains:annotations",
-		"com.google.code.gson:gson",
-		"net.kyori:",
-		"org.slf4j:",
-	)
-	relocations.forEach { relocate(it.key, "me.matin.mcore.libs.${it.value}") }
-	dependencies { exclusions.forEach { exclude(dependency(it)) } }
-	manifest { attributes["paperweight-mappings-namespace"] = "mojang" }
-	archiveFileName.set("${project.name}-${project.version}.jar")
-}
-
-tasks.jar {
-	manifest {
-		attributes["paperweight-mappings-namespace"] = "mojang"
-	}
-}
-
-tasks.build {
-	dependsOn(tasks.shadowJar)
+	compileOnly(kotlin("stdlib"))
+	compileOnly(kotlin("reflect"))
+	compileOnly(libs.bundles.kotlinx)
+	compileOnly(libs.bundles.orm)
+//	implementation("org.spongepowered:configurate-yaml:4.2.0")
+//	implementation("org.spongepowered:configurate-extra-kotlin:4.2.0")
+//	implementation("com.github.ItsDoot:configurate-serialization:0.1.1")
 }
 val javaVersion = 21
-
-tasks.processResources {
-	val ver = "version" to version.toString().replaceAfter('-', "SNAPSHOT")
-	inputs.properties(ver)
-	filteringCharset = "UTF-8"
-	filesMatching("plugin.yml") { expand(ver) }
-}
-
-tasks.withType<JavaCompile> {
-	options.encoding = "UTF-8"
-	options.release.set(javaVersion)
+tasks {
+	compileJava {
+		options.encoding = "UTF-8"
+		options.release.set(javaVersion)
+	}
+	
+	jar { manifest { attributes["paperweight-mappings-namespace"] = "mojang" } }
+	
+	shadowJar {
+		archiveClassifier = null
+		manifest.inheritFrom(jar.get().manifest)
+		val relocations = mapOf(
+			"me.matin.mlib" to "mlib",
+			"de.tr7zw.changeme.nbtapi" to "nbtapi",
+			//PacketEvents
+			"assets" to "packetevents.assets",
+			"com.github.retrooper.packetevents" to "packetevents.api",
+			"io.github.retrooper.packetevents" to "packetevents.impl",
+			//Configurate
+//			"org.spongepowered.configurate" to "configurate",
+//			"io.leangen.geantyref" to "configurate.geantyref",
+//			"pw.dotdash.configurate" to "configurate",
+		)
+		dependencies { exclude { it.moduleGroup == "net.kyori" } }
+		relocations.forEach { relocate(it.key, "me.matin.mcore.libs.${it.value}") }
+	}
+	
+	build { dependsOn(shadowJar) }
+	
+	processResources {
+		val versions = mapOf(
+			"version" to version.toString().replaceAfter('-', "SNAPSHOT"),
+			"kotlin" to libs.versions.kotlin.get(),
+			"coroutines" to libs.versions.kotlinx.coroutines.get(),
+			"serialization" to libs.versions.kotlinx.serialization.get(),
+			"exposed" to libs.versions.exposed.get(),
+			"mysql" to libs.versions.mysql.get()
+		)
+		inputs.properties(versions)
+		filteringCharset = "UTF-8"
+		filesMatching("plugin.yml") { expand(versions) }
+	}
 }
 
 kotlin {
