8db9e199bb52416cd69acba5411b94c5f2057dbb
Matin <65161829+Matin2@users.noreply.github.com>
2026-01-04 19:08:35 +0330

    Fix SkinsRestorer Hook & Move PlayerProfiles.kt to methods

--- src/main/kotlin/me/matin/mcore/Hooks.kt
+++ src/main/kotlin/me/matin/mcore/Hooks.kt
@@ -1,12 +1,23 @@
 package me.matin.mcore
 
 import me.matin.mcore.managers.hook.Hook
+import net.skinsrestorer.api.SkinsRestorer
+import net.skinsrestorer.api.SkinsRestorerProvider
 
 internal object Hooks {
 	
-	val skinsRestorer = Hook("SkinsRestorer", false)
+	object SkinsRestorerHook: Hook("SkinsRestorer", false) {
+		
+		var api: SkinsRestorer? = null
+			private set
+		
+		override suspend fun onInitialCheck() {
+			if (isHooked) api = SkinsRestorerProvider.get()
+			stateChanges.collect { api = if (it) SkinsRestorerProvider.get() else null }
+		}
+	}
 	
 	fun init() {
-		mcore.hooksHandler += skinsRestorer
+		mcore.hooksHandler += SkinsRestorerHook
 	}
 }
--- src/main/kotlin/me/matin/mcore/methods/PlayerProfiles.kt
+++ src/main/kotlin/me/matin/mcore/methods/PlayerProfiles.kt
@@ -1,15 +1,13 @@
-package me.matin.mcore.managers
+package me.matin.mcore.methods
 
-import com.destroystokyo.paper.profile.PlayerProfile
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.jsonPrimitive
 import me.matin.mcore.Hooks
 import net.skinsrestorer.api.PropertyUtils
-import net.skinsrestorer.api.SkinsRestorerProvider
-import org.bukkit.Bukkit.createProfile
+import org.bukkit.Bukkit
 import org.bukkit.OfflinePlayer
-import org.bukkit.profile.PlayerTextures.SkinModel
+import org.bukkit.profile.PlayerTextures
 import java.net.URI
 import java.util.*
 
@@ -26,31 +24,32 @@ object PlayerProfiles {
 			.jsonPrimitive.content
 	
 	/**
-	 * @param player Player witch you want the profile of.
-	 * @return [PlayerProfile] of the player with SkinsRestorer support.
+	 * @return [com.destroystokyo.paper.profile.PlayerProfile] of the player
+	 *    with SkinsRestorer support.
+	 * @receiver Player witch you want the profile of.
 	 */
 	@JvmStatic
-	operator fun get(player: OfflinePlayer) = if (Hooks.skinsRestorer.isHooked) SkinsRestorerProvider.get()
-		.playerStorage
-		.runCatching { getSkinForPlayer(player.uniqueId, player.name).get() }
-		.map { PropertyUtils.getSkinTextureUrl(it) }
-		.map { get(it, model = SkinModel.valueOf(PropertyUtils.getSkinVariant(it).name)) }
-		.getOrDefault(player.playerProfile)
-	else player.playerProfile
+	fun OfflinePlayer.getProfile() = Hooks.SkinsRestorerHook.api
+		?.playerStorage
+		?.runCatching { getSkinForPlayer(uniqueId, name).get() }
+		?.map { PropertyUtils.getSkinTextureUrl(it) }
+		?.map { get(it, model = PlayerTextures.SkinModel.valueOf(PropertyUtils.getSkinVariant(it).name)) }
+		?.getOrNull() ?: playerProfile
 	
 	/**
 	 * @param value URL or Base64 of the skin for the profile.
 	 * @param base64 (Optional) Whether [value] is Base64. defaults to `false`
 	 * @param model (Optional) Model of the skin. defaults to
-	 *    [SkinModel.CLASSIC]
-	 * @return [PlayerProfile] from the given url.
+	 *    [PlayerTextures.SkinModel.CLASSIC]
+	 * @return [com.destroystokyo.paper.profile.PlayerProfile] from the given
+	 *    url.
 	 */
 	@JvmStatic
 	operator fun get(
 		value: String,
 		base64: Boolean = false,
-		model: SkinModel = CLASSIC,
-	) = createProfile(UUID.randomUUID()).apply {
+		model: PlayerTextures.SkinModel = CLASSIC,
+	) = Bukkit.createProfile(UUID.randomUUID()).apply {
 		runCatching { URI(if (base64) value.url else value).toURL() }.onSuccess { textures.setSkin(it, model) }
 	}
 }
