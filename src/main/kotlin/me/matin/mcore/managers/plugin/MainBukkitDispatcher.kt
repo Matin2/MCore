02048c261d5e13012cece858ce2079753dcd04e3
Matin <65161829+Matin2@users.noreply.github.com>
2026-01-29 10:25:14 +0330

    Add delay support to MainBukkitDispatcher

--- src/main/kotlin/me/matin/mcore/managers/plugin/MainBukkitDispatcher.kt
+++ src/main/kotlin/me/matin/mcore/managers/plugin/MainBukkitDispatcher.kt
@@ -1,23 +1,27 @@
+@file:Suppress("unused")
+
 package me.matin.mcore.managers.plugin
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Runnable
-import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.*
 import me.matin.mcore.mcore
-import me.matin.mcore.methods.inTicks
 import org.bukkit.Bukkit
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.resume
-import kotlin.time.Duration
 
 
-object MainBukkitDispatcher : CoroutineDispatcher() {
+@OptIn(InternalCoroutinesApi::class)
+object MainBukkitDispatcher : CoroutineDispatcher(), Delay {
 	
 	internal lateinit var dispatcher: CoroutineDispatcher
 	
 	override fun dispatch(context: CoroutineContext, block: Runnable) = dispatcher.dispatch(context, block)
 	
-	override fun isDispatchNeeded(context: CoroutineContext) = !Bukkit.isPrimaryThread()
+	override fun scheduleResumeAfterDelay(timeMillis: Long, continuation: CancellableContinuation<Unit>) {
+		val ticks = (timeMillis / 50).coerceAtLeast(1)
+		Bukkit.getScheduler()
+			.runTaskLater(mcore.getOrThrow(), Runnable { continuation.resume(Unit) }, ticks)
+			.run { continuation.invokeOnCancellation { cancel() } }
+	}
 }
 
 suspend fun delayTicks(ticks: Long) = suspendCancellableCoroutine { cont ->
@@ -25,6 +29,3 @@ suspend fun delayTicks(ticks: Long) = suspendCancellableCoroutine { cont ->
 		.runTaskLater(mcore.getOrThrow(), Runnable { cont.resume(Unit) }, ticks)
 		.run { cont.invokeOnCancellation { cancel() } }
 }
-
-@Suppress("unused")
-suspend inline fun delayTicks(duration: Duration) = delayTicks(duration.inTicks)
