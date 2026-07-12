ee0fd9fff2f55ef7b4f3c09639ecac500978d93d
Matin <65161829+Matin2@users.noreply.github.com>
2026-07-12 21:47:48 +0330

    New ItemStack utils

---
+++ src/main/kotlin/com/github/matin2/mcore/methods/utils/Item.kt
@@ -0,0 +1,24 @@
+@file:Suppress("UnstableApiUsage", "NOTHING_TO_INLINE")
+
+package com.github.matin2.mcore.methods.utils
+
+import io.papermc.paper.datacomponent.DataComponentBuilder
+import io.papermc.paper.datacomponent.DataComponentType
+import org.bukkit.inventory.ItemStack
+
+/**
+ * Operator version of [setData].
+ *
+ * @param V value type
+ * @param data data to change
+ * @param value new value
+ * @see setData
+ */
+inline operator fun <V : Any> ItemStack.set(data: DataComponentType.Valued<V>, value: V) = setData(data, value)
+
+@Suppress("UnstableApiUsage")
+inline operator fun <V : Any> ItemStack.set(data: DataComponentType.Valued<V>, builder: DataComponentBuilder<V>) =
+	setData(data, builder)
+
+@Suppress("UnstableApiUsage")
+inline operator fun <V : Any> ItemStack.get(data: DataComponentType.Valued<V>) = getData(data)
