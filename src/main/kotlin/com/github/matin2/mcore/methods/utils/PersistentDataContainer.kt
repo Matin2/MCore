6d3a5df2793d020f5c2d179f72e9312ac81e36ef
Matin <65161829+Matin2@users.noreply.github.com>
2026-07-12 23:59:58 +0330

    Fixes and changes to PersistentDataContainer util

--- src/main/kotlin/com/github/matin2/mcore/methods/utils/PersistentDataContainer.kt
+++ src/main/kotlin/com/github/matin2/mcore/methods/utils/PersistentDataContainer.kt
@@ -14,38 +14,42 @@ import org.jetbrains.annotations.ApiStatus
 import kotlin.reflect.KType
 import kotlin.reflect.typeOf
 
-val persistentDataTypeMapper: Map<KType, PersistentDataType<out Any, out Any>>
-	field : MutableMap<KType, PersistentDataType<out Any, out Any>> = mutableMapOf(
-		typeOf<Byte>() to PersistentDataType.BYTE,
-		typeOf<Short>() to PersistentDataType.SHORT,
-		typeOf<Int>() to PersistentDataType.INTEGER,
-		typeOf<Long>() to PersistentDataType.LONG,
-		typeOf<Float>() to PersistentDataType.FLOAT,
-		typeOf<Double>() to PersistentDataType.DOUBLE,
-		typeOf<Boolean>() to PersistentDataType.BOOLEAN,
-		typeOf<String>() to PersistentDataType.STRING,
-		typeOf<ByteArray>() to PersistentDataType.BYTE_ARRAY,
-		typeOf<IntArray>() to PersistentDataType.INTEGER_ARRAY,
-		typeOf<LongArray>() to PersistentDataType.LONG_ARRAY,
-		typeOf<PersistentDataContainer>() to PersistentDataType.TAG_CONTAINER,
-		typeOf<List<Byte>>() to PersistentDataType.LIST.bytes(),
-		typeOf<List<Short>>() to PersistentDataType.LIST.shorts(),
-		typeOf<List<Int>>() to PersistentDataType.LIST.integers(),
-		typeOf<List<Long>>() to PersistentDataType.LIST.longs(),
-		typeOf<List<Float>>() to PersistentDataType.LIST.floats(),
-		typeOf<List<Double>>() to PersistentDataType.LIST.doubles(),
-		typeOf<List<Boolean>>() to PersistentDataType.LIST.booleans(),
-		typeOf<List<String>>() to PersistentDataType.LIST.strings(),
-		typeOf<List<ByteArray>>() to PersistentDataType.LIST.byteArrays(),
-		typeOf<List<IntArray>>() to PersistentDataType.LIST.integerArrays(),
-		typeOf<List<LongArray>>() to PersistentDataType.LIST.longArrays(),
-		typeOf<List<PersistentDataContainer>>() to PersistentDataType.LIST.dataContainers(),
-	)
+private val typeMapper: MutableMap<KType, PersistentDataType<out Any, out Any>> = mutableMapOf(
+	typeOf<Byte>() to PersistentDataType.BYTE,
+	typeOf<Short>() to PersistentDataType.SHORT,
+	typeOf<Int>() to PersistentDataType.INTEGER,
+	typeOf<Long>() to PersistentDataType.LONG,
+	typeOf<Float>() to PersistentDataType.FLOAT,
+	typeOf<Double>() to PersistentDataType.DOUBLE,
+	typeOf<Boolean>() to PersistentDataType.BOOLEAN,
+	typeOf<String>() to PersistentDataType.STRING,
+	typeOf<ByteArray>() to PersistentDataType.BYTE_ARRAY,
+	typeOf<IntArray>() to PersistentDataType.INTEGER_ARRAY,
+	typeOf<LongArray>() to PersistentDataType.LONG_ARRAY,
+	typeOf<PersistentDataContainer>() to PersistentDataType.TAG_CONTAINER,
+	typeOf<List<Byte>>() to PersistentDataType.LIST.bytes(),
+	typeOf<List<Short>>() to PersistentDataType.LIST.shorts(),
+	typeOf<List<Int>>() to PersistentDataType.LIST.integers(),
+	typeOf<List<Long>>() to PersistentDataType.LIST.longs(),
+	typeOf<List<Float>>() to PersistentDataType.LIST.floats(),
+	typeOf<List<Double>>() to PersistentDataType.LIST.doubles(),
+	typeOf<List<Boolean>>() to PersistentDataType.LIST.booleans(),
+	typeOf<List<String>>() to PersistentDataType.LIST.strings(),
+	typeOf<List<ByteArray>>() to PersistentDataType.LIST.byteArrays(),
+	typeOf<List<IntArray>>() to PersistentDataType.LIST.integerArrays(),
+	typeOf<List<LongArray>>() to PersistentDataType.LIST.longArrays(),
+	typeOf<List<PersistentDataContainer>>() to PersistentDataType.LIST.dataContainers(),
+)
 
 @ApiStatus.Internal
 fun registerPersistentDataType(type: PersistentDataType<out Any, out Any>, kType: KType) {
-	persistentDataTypeMapper[kType] = type
+	typeMapper[kType] = type
 }
+
+@ApiStatus.Internal
+@Suppress("UNCHECKED_CAST")
+fun <T : Any> getType(kType: KType) =
+	typeMapper[kType] as? PersistentDataType<Any, T> ?: throw IllegalArgumentException("Invalid Type")
 
 inline fun <reified T : Any> registerPersistentDataType(type: PersistentDataType<out Any, T>) =
 	registerPersistentDataType(type, typeOf<T>())
@@ -60,16 +64,18 @@ inline fun <reified P : Any, reified C : Any> registerPersistentDataType(
 	override fun fromPrimitive(primitive: P, context: PersistentDataAdapterContext) = unwrap(context, primitive)
 }.also { registerPersistentDataType(it) }
 
-inline fun ItemStack.container(crossinline block: PersistentDataContainer.() -> Unit) =
-	editPersistentDataContainer { block(it) }
+inline fun ItemStack.editContainer(crossinline block: PersistentDataContainer.() -> Unit) =
+	editPersistentDataContainer { it.block() }
 
-inline fun PersistentDataHolder.container(block: PersistentDataContainer.() -> Unit) =
-	persistentDataContainer.run(block)
+inline fun PersistentDataHolder.editContainer(block: PersistentDataContainer.() -> Unit) =
+	persistentDataContainer.block()
 
-inline fun TileState.container(block: PersistentDataContainer.() -> Unit) {
-	persistentDataContainer.run(block)
+inline fun TileState.editContainer(block: PersistentDataContainer.() -> Unit) {
+	persistentDataContainer.block()
 	update()
 }
+
+inline fun PersistentDataContainer.edit(block: PersistentDataContainer.() -> Unit) = block()
 
 inline fun PersistentDataContainer.addContainer(key: NamespacedKey, block: PersistentDataContainer.() -> Unit) {
 	set(key, PersistentDataType.TAG_CONTAINER, adapterContext.newPersistentDataContainer().apply(block))
@@ -77,20 +83,16 @@ inline fun PersistentDataContainer.addContainer(key: NamespacedKey, block: Persi
 
 inline fun PersistentDataContainerView.getContainer(key: NamespacedKey) = get(key, PersistentDataType.TAG_CONTAINER)
 
-@Suppress("UNCHECKED_CAST")
 inline operator fun <reified V : Any> PersistentDataContainer.set(key: NamespacedKey, value: V) =
-	set(key, persistentDataTypeMapper[typeOf<V>()] as PersistentDataType<Any, V>, value)
+	set(key, getType(typeOf<V>()), value)
 
-@Suppress("UNCHECKED_CAST")
 inline operator fun <reified V : Any> PersistentDataContainerView.get(key: NamespacedKey) =
-	get(key, persistentDataTypeMapper[typeOf<V>()] as PersistentDataType<Any, V>)
+	get(key, getType<V>(typeOf<V>()))
 
-@Suppress("UNCHECKED_CAST")
 inline fun <reified V : Any> PersistentDataContainerView.getOrDefault(key: NamespacedKey, default: V) =
-	getOrDefault(key, persistentDataTypeMapper[typeOf<V>()] as PersistentDataType<Any, V>, default)
+	getOrDefault(key, getType(typeOf<V>()), default)
 
-@Suppress("UNCHECKED_CAST")
 inline fun <reified V : Any> PersistentDataContainerView.has(key: NamespacedKey) =
-	has(key, persistentDataTypeMapper[typeOf<V>()] as PersistentDataType<Any, V>)
+	has(key, getType(typeOf<V>()))
 
 inline operator fun PersistentDataContainerView.contains(key: NamespacedKey) = has(key)
