11c7eaec02dac5256f937e177fa4a6de61cebe19
Matin <65161829+Matin2@users.noreply.github.com>
2026-08-03 11:36:38 +0330

    [CommandAPI] Disable adding external literals to literal

--- src/main/kotlin/services/command/CommandLiteralContext.kt
+++ src/main/kotlin/services/command/CommandLiteralContext.kt
@@ -1,10 +1,8 @@
 package com.github.matin2.mcore.services.command
 
 import com.github.matin2.mcore.services.command.execution.CommandExecution
-import com.github.matin2.mcore.services.command.execution.CommandSourcePredicate
 import io.papermc.paper.command.brigadier.Commands
 import kotlinx.coroutines.CoroutineScope
-import org.jetbrains.annotations.ApiStatus
 
 @CommandDsl
 @Suppress("NOTHING_TO_INLINE", "unused")
@@ -26,8 +24,8 @@ class CommandLiteralContext(name: String, vararg val aliases: String) {
 		scopeSetters.forEach { it(scope) }
 	}
 	
-	@ApiStatus.Internal
-	fun build() = CommandLiteral(buildList {
+	@PublishedApi
+	internal fun build() = CommandLiteral(buildList {
 		val main = builder.requires { source ->
 			executes.executors.any { it.condition(source) } && requirements.all { it(source) }
 		}.executes(executes.build { scope }).build()
@@ -35,14 +33,18 @@ class CommandLiteralContext(name: String, vararg val aliases: String) {
 		aliases.mapTo(this) { Commands.literal(it).redirect(main).build() }
 	}, if (::description.isInitialized) description else null, ::setScope)
 	
-	inline operator fun String.invoke(action: CommandLiteralContext.() -> Unit) =
-		+command(this, action = action)
-	
-	inline operator fun List<String>.invoke(action: CommandLiteralContext.() -> Unit) =
-		+command(first(), aliases = drop(1).toTypedArray(), action)
-	
-	operator fun CommandLiteral.unaryPlus() {
+	@PublishedApi
+	internal fun CommandLiteral.asChild() {
 		scopeSetters += setScope
 		nodes.forEach { builder.then(it) }
 	}
+	
+	inline fun literal(name: String, vararg aliases: String, action: CommandLiteralContext.() -> Unit) =
+		command(name, aliases = aliases, action).asChild()
+	
+	inline operator fun String.invoke(noinline action: CommandLiteralContext.() -> Unit) =
+		literal(this, action = action)
+	
+	inline operator fun List<String>.invoke(noinline action: CommandLiteralContext.() -> Unit) =
+		literal(first(), aliases = drop(1).toTypedArray(), action)
 }
